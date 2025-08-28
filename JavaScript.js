b0bb78f3da4d6ae7bb9314383876d597597e2f4c
Kaleb <kalebmulugeta07@gmail.com>
2025-08-28 14:13:11 -0500

    Set minimum date for date inputs to today

--- JavaScript.js
+++ JavaScript.js
@@ -208,4 +208,9 @@ document.addEventListener("DOMContentLoaded", () => {
     observer.observe(el)
   })
 
+    // Set minimum date for date inputs to today
+  const today = new Date().toISOString().split("T")[0]
+  document.getElementById("checkin").setAttribute("min", today)
+  document.getElementById("checkout").setAttribute("min", today)
+
 })
