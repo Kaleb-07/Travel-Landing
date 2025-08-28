95e866c39db7e5c382a5bec598be2494f94af7e9
Kaleb <kalebmulugeta07@gmail.com>
2025-08-28 14:09:24 -0500

    intersectionObserver

--- JavaScript.js
+++ JavaScript.js
@@ -190,4 +190,12 @@ document.addEventListener("DOMContentLoaded", () => {
     rootMargin: "0px 0px -50px 0px",
   }
 
+    const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1"
+        entry.target.style.transform = "translateY(0)"
+      }
+    })
+  }, observerOptions)
 })
