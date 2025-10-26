80d0cd87e50eceb05a769314c668c2030bbbb423
Kaleb <kalebmulugeta07@gmail.com>
2025-10-26 14:04:25 -0500

    Toggle password visibility

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -237,4 +237,22 @@ function isStrongPassword(password) {
   return passwordRegex.test(password)
 }
 
+// Toggle password visibility
+function togglePassword(inputId) {
+  const input = document.getElementById(inputId)
+  const button = event.target.closest(".toggle-password")
+  const icon = button.querySelector("i")
+
+  if (input.type === "password") {
+    input.type = "text"
+    icon.classList.remove("fa-eye")
+    icon.classList.add("fa-eye-slash")
+  } else {
+    input.type = "password"
+    icon.classList.remove("fa-eye-slash")
+    icon.classList.add("fa-eye")
+  }
+}
+
+
 
